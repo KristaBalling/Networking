e415d532851369c9505bea58593e84bd9f703786
Krista Blachian <krista.blachian@scifusion360.com>
2019-02-27 15:53:35 -0500

    prints key & value pairs

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -3,27 +3,40 @@ package com.theironyard;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.net.*;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
 
             BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(url.openStream()));
+                    new InputStreamReader(urlConnection.getInputStream()));
 
-                    String line = "";
-                    while(line != null) {
-                        line = inputStream.readLine();
-                        System.out.println(line);
-                    }
-                    inputStream.close();
-        } catch(IOException e) {
+            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+            for(Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
+                String key = entry.getKey();
+                List<String> value = entry.getValue();
+                System.out.println("-----key = " + key);
+                for(String string: value) {
+                    System.out.println("value = " + value);
+                }
+            }
+//                    String line = "";
+//                    while(line != null) {
+//                        line = inputStream.readLine();
+//                        System.out.println(line);
+//                    }
+//                    inputStream.close();
+        } catch (MalformedURLException e) {
+            System.out.println("URL Malformed: " + e.getMessage());
+        } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
 
 //            URI uri = url.toURI();
@@ -61,9 +74,7 @@ public class Main {
 //        } catch (
 //                URISyntaxException e) {
 //            System.out.println("URI Bad Syntax: " + e.getMessage());
-//        } catch (
-//                MalformedURLException e) {
-//            System.out.println("URL Malformed: " + e.getMessage());
+
         }
     }
 }

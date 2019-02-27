4cea695c04f17ccfacf0813496b17ac82302401b
Krista Blachian <krista.blachian@scifusion360.com>
2019-02-27 14:32:56 -0500

    url to uri

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -9,26 +9,29 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            URI baseUri = new URI("http://username:password@myserver.com:5000");
-            URI uri1 = new URI("/catalogue/phones?os=android#samsung");
-            URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung");
-            URI uri3 = new URI("/stores/locations?zip=12345");
+            URL url = new URL("http://example.org");
+            URI uri = url.toURI();
 
-            URI resolvedUri1 = baseUri.resolve(uri1);
-            URI resolvedUri2 = baseUri.resolve(uri2);
-            URI resolvedUri3 = baseUri.resolve(uri3);
-
-            URL url1 = resolvedUri1.toURL();
-            System.out.println("URL = " + url1);
-            URL url2 = resolvedUri2.toURL();
-            System.out.println("URL = " + url2);
-            URL url3 = resolvedUri3.toURL();
-            System.out.println("URL = " + url3);
-
-            URI relativizedURI = baseUri.relativize(resolvedUri2);
-            System.out.println("Relative URI = " + relativizedURI);
-
-
+//            URI baseUri = new URI("http://username:password@myserver.com:5000");
+//            URI uri1 = new URI("/catalogue/phones?os=android#samsung");
+//            URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung");
+//            URI uri3 = new URI("/stores/locations?zip=12345");
+//
+//            URI resolvedUri1 = baseUri.resolve(uri1);
+//            URI resolvedUri2 = baseUri.resolve(uri2);
+//            URI resolvedUri3 = baseUri.resolve(uri3);
+//
+//            URL url1 = resolvedUri1.toURL();
+//            System.out.println("URL = " + url1);
+//            URL url2 = resolvedUri2.toURL();
+//            System.out.println("URL = " + url2);
+//            URL url3 = resolvedUri3.toURL();
+//            System.out.println("URL = " + url3);
+//
+//            URI relativizedURI = baseUri.relativize(resolvedUri2);
+//            System.out.println("Relative URI = " + relativizedURI);
+//
+//
         } catch (
                 URISyntaxException e) {
             System.out.println("URI Bad Syntax: " + e.getMessage());

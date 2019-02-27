f70b5d786965b11f7761ff26c3aa9090425530cc
Krista Blachian <krista.blachian@scifusion360.com>
2019-02-27 14:58:01 -0500

    input stream prints out html

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,5 +1,8 @@
 package com.theironyard;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -10,17 +13,30 @@ public class Main {
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org");
-            URI uri = url.toURI();
 
-            System.out.println("Scheme = " + uri.getScheme());
-            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-            System.out.println("Authority = " + uri.getAuthority());
-            System.out.println("User Info = " + uri.getUserInfo());
-            System.out.println("Host = " + uri.getHost());
-            System.out.println("Port = " + uri.getPort());
-            System.out.println("Path = " + uri.getPath());
-            System.out.println("Query = " + uri.getQuery());
-            System.out.println("Fragment = " + uri.getFragment());
+            BufferedReader inputStream = new BufferedReader(
+                    new InputStreamReader(url.openStream()));
+
+                    String line = "";
+                    while(line != null) {
+                        line = inputStream.readLine();
+                        System.out.println(line);
+                    }
+                    inputStream.close();
+        } catch(IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+
+//            URI uri = url.toURI();
+//
+//            System.out.println("Scheme = " + uri.getScheme());
+//            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+//            System.out.println("Authority = " + uri.getAuthority());
+//            System.out.println("User Info = " + uri.getUserInfo());
+//            System.out.println("Host = " + uri.getHost());
+//            System.out.println("Port = " + uri.getPort());
+//            System.out.println("Path = " + uri.getPath());
+//            System.out.println("Query = " + uri.getQuery());
+//            System.out.println("Fragment = " + uri.getFragment());
 
 //            URI baseUri = new URI("http://username:password@myserver.com:5000");
 //            URI uri1 = new URI("/catalogue/phones?os=android#samsung");
@@ -42,12 +58,12 @@ public class Main {
 //            System.out.println("Relative URI = " + relativizedURI);
 //
 //
-        } catch (
-                URISyntaxException e) {
-            System.out.println("URI Bad Syntax: " + e.getMessage());
-        } catch (
-                MalformedURLException e) {
-            System.out.println("URL Malformed: " + e.getMessage());
+//        } catch (
+//                URISyntaxException e) {
+//            System.out.println("URI Bad Syntax: " + e.getMessage());
+//        } catch (
+//                MalformedURLException e) {
+//            System.out.println("URL Malformed: " + e.getMessage());
         }
     }
 }
